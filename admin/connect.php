2a95894893fd3d906672cedad00a860117d1e261
itshieeus <vanhieu3009@gmail.com>
2023-07-03 07:39:37 +0700

    CODE

---
+++ admin/connect.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "tichhop";
+$conn = mysqli_connect($hostname, $username, $password, $database);
+mysqli_set_charset($conn, 'UTF8');
+?>
